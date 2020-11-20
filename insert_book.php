4e0b9da78e80962fbbe5d0cca0b6c8eb94d0d97e
joel2usha-rgb <66008071+joel2usha-rgb@users.noreply.github.com>
2020-11-20 14:47:53 +0800

    Update insert_book.php

--- insert_book.php
+++ insert_book.php
@@ -9,22 +9,42 @@
     <h1>Book Entry Results</h1>
     <?php
     // TODO 1: Create short variable names.
+     $isbn = $_POST['isbn'];
+     $author = $_POST['author'];
+     $title = $_POST['title'];
+     $price = $_POST['price'];
 
+     
 
     // TODO 2: Check and filter data coming from the user.
-
-
+    if ($isbn == null || $author == null || $title == null || $price == null)
+        die("All fields must not be empty");
+        
     // TODO 3: Setup a connection to the appropriate database.
-
+    require_once 'login.php';
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if($conn->connect_error) die ("Fatal Error");
 
     // TODO 4: Query the database.
-
+    $query="INSERT INTO catalogs (isbn, author, title, price) VALUES ('$isbn', '$author', '$title',  '$price')";
+    $result = $conn->query($query);
+   
 
     // TODO 5: Display the feedback back to user.
+    
+    
+      
+    
+     
+      if (!$result) die("Fatal error, data is not inserted.");
+      echo "Added successfully";
+  
+  
 
 
     // TODO 6: Disconnecting from the database.
-
+    
+    $conn->close();
 
     ?>
 </body>
